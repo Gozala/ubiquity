4b636434180c5b85db4fdbfb2c136b26d628c6cf
Irakli Gozalishvili <rfobic@gmail.com>
2011-06-21 19:03:29 +0200

    Removing unrelated stuff from main module.

--- ubiquity.js
+++ ubiquity.js
@@ -7,33 +7,6 @@
 
 "use strict";
 
-const { Cc, Ci } = require('chrome')
-const awesomebar = require('https!raw.github.com/autonome/Jetpack-Modules/master/awesomebar')
-const { tagURI, untagURI } = Cc['@mozilla.org/browser/tagging-service;1'].
-             getService(Ci.nsITaggingService)
-const tabs = require('tabs');
-
-awesomebar.register(function tag(query) {
-  let tags = query.split(/[,|\s]+/)
-  return function stream(next, stop) {
-    if (!tags.length) return stop()
-    stop(next({
-      icon: 'chrome://browser/skin/places/star-icons.png',
-      title: 'Tags: ' + tags.join(', '),
-      url: ''
-    }))
-    console.log('<<<<<')
-  }
-})
-awesomebar.register(function untag() {
-  return function stream(next, stop) {
-    next({
-      icon: 'chrome://browser/skin/places/star-icons.png',
-      title: 'Untag this page',
-      url: ''
-    })
-    stop()
-  }
-})
+exports.Parser = require('./parser');
 
 })
